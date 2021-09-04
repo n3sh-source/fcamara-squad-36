f57bd882ff8592c59e8434d556f0b570949374cb
xUrso <42844085+xUrso@users.noreply.github.com>
2021-09-03 23:31:19 -0300

    Escopo inicial do script de login

---
+++ login.js
@@ -0,0 +1,20 @@
+var tentativa = 3;
+function validar(){
+var email = document.getElementById("email").value;
+var password = document.getElementById("password").value;
+if ( email == "gustavo@fcamara.com" && password == "sanguelaranja"){
+alert ("Login bem-sucedido");
+window.location = "success.html";
+return false;
+}
+else{
+tentativa --;
+alert("Voce saiu "+tentativa+" tentativa;");
+if( attempt == 0){
+document.getElementById("input-email").disabled = true;
+document.getElementById("input-password").disabled = true;
+document.getElementById("botao").disabled = true;
+return false;
+}
+}
+}
