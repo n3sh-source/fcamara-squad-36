1063b55749557dbe29d76207e1089eb860f4fb01
Gustavo Prado <gustavopradohenrique@gmail.com>
2021-09-09 15:39:18 -0300

    modificação no codigo

--- login.js
+++ login.js
@@ -1,20 +1,87 @@
-var tentativa = 3;
-function validar(){
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
+const fields = document.querySelectorAll("[required]")
 
-    if ( email == "gustavo@fcamara.com" && password == "sanguelaranja"){
-        alert ("Login bem-sucedido");
-        window.location = "success.html";
-        return false;
-    }else{
-        tentativa --;
-        alert("Voce saiu "+tentativa+" tentativa;");
-        if( attempt == 0){
-            document.getElementById("input-email").disabled = true;
-            document.getElementById("input-password").disabled = true;
-            document.getElementById("botao").disabled = true;
-            return false;
+function ValidateField(field) {
+    // logica para verificar se existem erros
+    function verifyErrors() {
+        let foundError = false;
+
+        for(let error in field.validity) {
+            // se não for customError
+            // então verifica se tem erro
+            if (field.validity[error] && !field.validity.valid ) {
+                foundError = error
+            }
+        }
+        return foundError;
+    }
+
+    function customMessage(typeError) {
+        const messages = {
+            text: {
+                valueMissing: "Por favor, preencha este campo"
+            },
+            email: {
+                valueMissing: "Email é obrigatório",
+                typeMismatch: "Por favor, preencha um email válido"
+            }
+        }
+
+        return messages[field.type][typeError]
+    }
+
+    function setCustomMessage(message) {
+        const spanError = field.parentNode.querySelector("span.error")
+
+        if (message) {
+            spanError.classList.add("active")
+            spanError.innerHTML = message
+        } else {
+            spanError.classList.remove("active")
+            spanError.innerHTML = ""
+        }
+    }
+
+    return function() {
+
+        const error = verifyErrors()
+
+        if(error) {
+            const message = customMessage(error)
+
+            field.style.borderColor = "red"
+            setCustomMessage(message)
+        } else {
+            field.style.borderColor = "green"
+            setCustomMessage()
         }
     }
 }
+
+
+function customValidation(event) {
+
+    const field = event.target
+    const validation = ValidateField(field)
+
+    validation()
+
+}
+
+for( field of fields ){
+    field.addEventListener("invalid", event => {
+        // eliminar o bubble
+        event.preventDefault()
+
+        customValidation(event)
+    })
+    field.addEventListener("blur", customValidation)
+}
+
+
+document.querySelector("form")
+.addEventListener("submit", event => {
+    console.log("enviar o formulário")
+
+    // não vai enviar o formulário
+    event.preventDefault()
+})
