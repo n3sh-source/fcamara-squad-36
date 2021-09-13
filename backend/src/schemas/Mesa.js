f4cba875573b4c3fe8a8f65be3364e7a922a6ff9
joserx <jose.hamilton2050@gmail.com>
2021-09-12 23:27:35 -0300

    Implementação do Codigo da mesa

--- backend/src/schemas/Mesa.js
+++ backend/src/schemas/Mesa.js
@@ -8,14 +8,23 @@ const MesaSchema = new mongoose.Schema({
   },
   vagas: {
     type: Number,
-    default: 4 
+    default: 4,
   },
   filial: { ref: "Filial", type: mongoose.Types.ObjectId, required: true },
   deleted: {
     type: Boolean,
     default: false,
   },
-  
+  codigo: {
+    type: Number,
+    unique: true,
+  },
 });
-mongoose.model("Mesa", MesaSchema);
 
+MesaSchema.pre("save", async function () {
+  const time = String(new Date().getTime());
+  const codigo = time.substring(5, 11);
+  this.codigo = codigo;
+});
+
+mongoose.model("Mesa", MesaSchema);
