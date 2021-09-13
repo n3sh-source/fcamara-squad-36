c8756afe1cbf4db4127e2da656d38e475fb1dc0c
joserx <jose.hamilton2050@gmail.com>
2021-09-13 01:33:52 -0300

    Tratativa de Erros + Validação de Reserva

--- backend/src/routes/consultor.routes.js
+++ backend/src/routes/consultor.routes.js
@@ -1,39 +1,53 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const routes = express.Router();
+const consultorSchema = mongoose.model("Consultor");
 
 routes.get("/", async (req, res) => {
-  const consultorSchema = mongoose.model("Consultor");
-  const consultores = await consultorSchema
-    .find({
-      deleted: false,
-    })
-    .populate("filial", "-__v")
-    .select("-__v");
-  res.send(consultores);
+  try {
+    const consultores = await consultorSchema
+      .find({
+        deleted: false,
+      })
+      .populate("filial", "-__v")
+      .select("-__v");
+    res.send(consultores);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
 });
 routes.post("/", async (req, res) => {
-  const consultorSchema = mongoose.model("Consultor");
-  const consultor = await consultorSchema.create(req.body);
-  res.send(consultor);
+  try {
+    const consultor = await consultorSchema.create(req.body);
+    res.send(consultor);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
 });
 routes.put("/:id", async (req, res) => {
-  const consultorSchema = mongoose.model("Consultor");
-  const consultor = await consultorSchema.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    {
-      new: true,
-    }
-  );
-  res.send(consultor);
+  try {
+    const consultor = await consultorSchema.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+      }
+    );
+    res.send(consultor);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
 });
 routes.delete("/:id", async (req, res) => {
-  const consultorSchema = mongoose.model("Consultor");
-  const consultor = await consultorSchema.findByIdAndUpdate(req.params.id, {
-    deleted: true,
-  });
-  res.send(consultor);
+  try {
+    const consultorSchema = mongoose.model("Consultor");
+    const consultor = await consultorSchema.findByIdAndUpdate(req.params.id, {
+      deleted: true,
+    });
+    res.send(consultor);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
 });
 
 module.exports = routes;
--- backend/src/routes/mesa.routes.js
+++ backend/src/routes/mesa.routes.js
@@ -1,35 +1,48 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const routes = express.Router();
+const mesaSchema = mongoose.model("Mesa");
 
 routes.get("/", async (req, res) => {
-  const mesaSchema = mongoose.model("Mesa");
-  const mesas = await mesaSchema
-    .find({
-      deleted: false,
-    })
-    .populate("filial", "-__v")
-    .select("-__v");
-  res.send(mesas);
+  try {
+    const mesas = await mesaSchema
+      .find({
+        deleted: false,
+      })
+      .populate("filial", "-__v")
+      .select("-__v");
+    res.send(mesas);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
 });
 routes.post("/", async (req, res) => {
-  const mesaSchema = mongoose.model("Mesa");
-  const mesa = await mesaSchema.create(req.body);
-  res.send(mesa);
+  try {
+    const mesa = await mesaSchema.create(req.body);
+    res.send(mesa);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
 });
 routes.put("/:id", async (req, res) => {
-  const mesaSchema = mongoose.model("Mesa");
-  const mesa = await mesaSchema.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-  res.send(mesa);
+  try {
+    const mesa = await mesaSchema.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.send(mesa);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
 });
 routes.delete("/:id", async (req, res) => {
-  const mesaSchema = mongoose.model("Mesa");
-  const mesa = await mesaSchema.findByIdAndUpdate(req.params.id, {
-    deleted: true,
-  });
-  res.send(mesa);
+  try {
+    const mesa = await mesaSchema.findByIdAndUpdate(req.params.id, {
+      deleted: true,
+    });
+    res.send(mesa);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
 });
 
 module.exports = routes;
