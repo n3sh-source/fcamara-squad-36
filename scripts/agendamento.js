f31eec38626d99a3d3e53d67371dfb464fce76e4
joserx <jose.hamilton2050@gmail.com>
2021-09-15 03:23:17 -0300

    Conexão das telas com backend

---
+++ scripts/agendamento.js
@@ -0,0 +1,49 @@
+fetch("http://localhost:3000/mesa", {
+  method: "GET",
+  headers: { "Content-Type": "application/json" },
+})
+  .then(async (resposta) => {
+    resposta = await resposta.json();
+    const select = document.getElementById("mesas");
+    for (const mesa of resposta) {
+      const option = document.createElement("option");
+      option.setAttribute("label", mesa.nome);
+      option.setAttribute("value", mesa._id);
+
+      select.appendChild(option);
+    }
+  })
+  .catch((error) => {
+    alert("Email e ou senha invalidos");
+  });
+
+const agendar = document.getElementById("agendar");
+agendar.addEventListener("click", (evento) => {
+  const data = document.getElementById("data").value;
+  const ano = data.substring(0, 4);
+  const mes = data.substring(5, 7);
+  const dia = data.substring(8, 10);
+  const novaData = new Date(ano, mes, dia);
+  const select = document.getElementById("mesas");
+  const mesaId = select.options[select.selectedIndex].value;
+  const consultor = JSON.parse(localStorage.getItem("consultor"));
+
+  const dados = { mesa: mesaId, data: novaData, consultor: consultor._id };
+  fetch("http://localhost:3000/reserva", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(dados),
+  })
+    .then(async (resposta) => {
+      if (resposta.status != 200) {
+        resposta = await resposta.json();
+        return alert(resposta.erro);
+      }
+      localStorage.setItem("reserva", JSON.stringify(dados));
+      alert("Reserva agendada com sucesso");
+      window.location.href = "./reserva.html";
+    })
+    .catch((error) => {
+      alert("Verifique todos os campos e tente novamente");
+    });
+});
---
+++ scripts/reserva.js
@@ -0,0 +1,15 @@
+const reserva = JSON.parse(localStorage.getItem("reserva"));
+const resposta = document.getElementById("resposta");
+const diaReserva = reserva.data.substring(0, 10);
+
+let dia = "";
+const day = new Date(reserva.data).getDay();
+if (day == 0) dia = "Domingo";
+if (day == 1) dia = "Segunda-Feira";
+if (day == 2) dia = "Terça-Feira";
+if (day == 3) dia = "Quarta-Feira";
+if (day == 4) dia = "Quinta-Feira";
+if (day == 5) dia = "Sexta-Feira";
+if (day == 6) dia = "Sabádo";
+
+resposta.innerHTML = diaReserva + " " + dia;
